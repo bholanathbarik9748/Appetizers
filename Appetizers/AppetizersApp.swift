a5a79a1b8609f99f1d81d9161c91e73af56fec69
Bholanath <bholanathbarik9748@gmail.com>
2024-06-15 16:49:33 +0530

    create project

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Bholanath Barik on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
